ce972219b0e9889cd34919ca835d52d0f636fd1c
bigfang <bitair@gmail.com>
2021-07-01 17:00:08 +0800

    tmp

--- src/app/routes/wisdom/wisdom.module.ts
+++ src/app/routes/wisdom/wisdom.module.ts
@@ -2,8 +2,10 @@ import { NgModule, Type } from '@angular/core';
 import { SharedModule } from '@shared';
 import { WisdomRoutingModule } from './wisdom-routing.module';
 import { WisdomEnterpriseComponent } from './enterprise/enterprise.component';
+import { WisdomEnterpriseEditComponent } from './enterprise/edit/edit.component';
+import { WisdomEnterpriseViewComponent } from './enterprise/view/view.component';
 
-const COMPONENTS: Type<void>[] = [WisdomEnterpriseComponent];
+const COMPONENTS: Type<void>[] = [WisdomEnterpriseComponent, WisdomEnterpriseEditComponent, WisdomEnterpriseViewComponent];
 
 @NgModule({
   imports: [SharedModule, WisdomRoutingModule],
